649a4cae0891b4267f5d7973a97ae3bfe66efe43
M-Pruidze <m.pruidze.exceedteam@gmail.com>
2021-10-11 15:01:34 +0400

    fix server

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
+app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
+
+const port = process.env.PORT || "3000";
+
+app.listen(port, () => {
+  console.log(`listen on `, port )
+})
+
+module.exports = app;
